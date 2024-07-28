142071184f5e81c2261f06c9ede8f13954cffbd2
gnajor <lejonet04@gmail.com>
2024-07-28 16:57:02 +0200

    added interactive data

--- API/hacking_content.php
+++ API/hacking_content.php
@@ -1,6 +1,32 @@
 <?php
 
 
+function get_interactive_data($row_amount, $column_amount, $word_amount, $letter_amount){
+    $chars_array = json_decode(file_get_contents("database/chars.json"), true);
+    $words_array = json_decode(file_get_contents("database/words.json"), true);
+    $filtererd_word_array = filter_words_by_letter_amount($letter_amount, $words_array);
+    $special_chars = $chars_array[1]["specials"];
+
+    $char_amount = $column_amount * $row_amount;
+    $char_amount_with_words = $char_amount - ($word_amount * $letter_amount);
+
+    $words = [];
+    for($i = 0; $i < $word_amount; $i++){
+        $random_index = rand(0, count($filtererd_word_array) - 1);
+        $random_word = $filtererd_word_array[$random_index];
+        $words[] = $random_word;
+    }
+
+    $chars = [];
+    for($i = 0; $i < $char_amount_with_words; $i++){
+        $random_index = rand(0, count($special_chars) - 1);
+        $random_char = $special_chars[$random_index];
+        $chars[] = $random_char;
+    }
+
+    return ["words" => $words, "special_chars" => $chars];
+}
+
 function get_non_interactive_data($data_amount){
     $chars_array = json_decode(file_get_contents("database/chars.json"), true);
     $letters_nums_array = $chars_array[0]["letters_nums"];
@@ -13,12 +39,11 @@ function get_non_interactive_data($data_amount){
         $random_index_2 = $random_index_2 - 2; 
     }
 
-
     $random_char_1 = $letters_nums_array[$random_index_1];
     $random_char_2 = $letters_nums_array[$random_index_2];
     
     $non_interactive_chars = [];
-    $random_row = rand(6, 25);
+    $random_row = rand(6, 20);
 
     for($row_index = 0; $row_index < $data_amount; $row_index++){
         $random_index_3 = rand(0, count($letters_nums_array) - 1);
@@ -43,8 +68,24 @@ function get_non_interactive_data($data_amount){
 
     return $non_interactive_chars;
 }
+
+
+function filter_words_by_letter_amount($letter_amount, $words){
+    $result = [];
+
+    foreach($words as $word){
+        if(strlen($word) === $letter_amount){
+            $result[] = $word;
+        }
+    }
+    return $result;
+}
+
+$send_array = get_interactive_data(32, 12, 8, 4);
+$send_array["non_interactive"] = get_non_interactive_data(32);
+
 echo "<pre>";
-echo var_dump(get_non_interactive_data(32));
+echo var_dump($send_array);
 echo "</pre>";
 
 ?>
